faf8f1b019220e736615b5f0db8249acfd2f89bb
JediMasterLucy55 <195190504+JediMasterLucy55@users.noreply.github.com>
2026-04-06 17:32:25 -0500

    done it works
    its lowkey homophobic LMAO

--- book_classification.py
+++ book_classification.py
@@ -1,22 +1,17 @@
 import json
-import os
-import pickle
-import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.pipeline import Pipeline
 
 # loads the JSON file
-with open('books.json', 'r') as file:
-    data = json.load(file)
+with open('books.json', 'r') as f:
+    data = json.load(f)
 
-synopsis = [book["synopsis"] for book in data]
-genre = [book["genre"] for book in data]
+X = [book["synopsis"] for book in data]
+y = [book["genre"] for book in data]
 
-X_train, Y_train, X_test, Y_test = train_test_split(
-    synopsis, genre, train_size = 0.2, random_state = 42
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.2, random_state = 42
 )
 
 vectorizer = TfidfVectorizer()
@@ -25,7 +20,7 @@ X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)    
 
 model = LogisticRegression(max_iter=1000)  
-model.fit(X_train_tfidf, Y_train)  
+model.fit(X_train_tfidf, y_train)  
 
 prediction = model.predict(X_test_tfidf)
 
