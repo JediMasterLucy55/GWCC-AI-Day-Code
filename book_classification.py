68122fae2643d784bc18ec5b3fd8aa8e94097f76
JediMasterLucy55 <195190504+JediMasterLucy55@users.noreply.github.com>
2026-04-06 11:26:16 -0500

    json and basic stuffs

--- book_classification.py
+++ book_classification.py
@@ -1,3 +1,11 @@
-# pip install scikit-learn
-import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split
 
+vectorizer = TfidfVectorizer()
+
+data = "books.json"
+
+matrix = vectorizer.fit_transform(data)
+
+print(matrix.toArray())
